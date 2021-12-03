165cf0e290a0636226d24e8304d04149f2286419
Carlos Fabbri Jr <cfabbrijr@gmail.com>
2021-12-03 14:25:54 -0300

    Add part 1 folder with courseinfo step1 exercise solution

---
+++ Part1/part1/courseinfo/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+// Code developed by Carlos Fabbri Jr for the Course FSOpen2021
+//Date : December 02, 2021
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p> Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </div>
+  )
+}
+
+
+const App = () => {
+
+  return (
+    <div>
+      <Header course = 'Half Stack application development'/>
+      <Content part = 'Fundamentals of React' exercises = {10}/>
+      <Content part = 'Using props to pass data' exercises = {7} />
+      <Content part = 'State of a component' exercises = {14} />
+      <Total exercises1 = {10} exercises2 = {7} exercises3 = {14}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Part1/part1/courseinfo/src/index.js
@@ -0,0 +1,10 @@
+//Code developed by Carlos Fabbri Jr  for the course FSOpen2021
+//Date : December 02, 2021
+
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
