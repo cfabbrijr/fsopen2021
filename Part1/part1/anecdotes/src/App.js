83fbb2fd7ae696c1dbabcc42ebf85794f787effa
Carlos Fabbri Jr <cfabbrijr@gmail.com>
2021-12-16 17:06:21 -0300

    Last change on exercise_1.13 code

--- Part1/part1/anecdotes/src/App.js
+++ Part1/part1/anecdotes/src/App.js
@@ -14,36 +14,59 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
 
-  const [selected, setSelected] = useState(0)
-  const [allVotes, setAll] = useState([])
+  const [selected, setSelected] = useState(0) // this is the chosen anecdote shown on top of page
+  const [allVotes, setAll] = useState([]) // this stores the votes given by the user
+  const [maisVotada, setMaisVotada] = useState([]) // this stores the number of most voted anecdote
+  const [numeroVotos, setNumeroV] = useState([]) // this stores the number of votes for each anecdote
+  const contando = [...allVotes]
+  let maior = -Infinity;
+
 
   const handleRandomAnecdote = () => {
     // this function generates random numbers between 0 and anecdotes.lenght minus one, due to Math.random limits
     // In this case between 0 and 6, which are the index of each anecdote on anecdotes array
-    setSelected(Math.floor(Math.random()*(anecdotes.length)))
+    setSelected(Math.floor(Math.random()*anecdotes.length))
   }
 
   const handleVotes = () => {
     // This function populates the allVotes array with the index of the voted anecdote
     setAll(allVotes.concat(selected))
-  }
-
-  const counts = {};
-  // this function register the frequency of each number voted
+      // this function register the frequency of each number voted
     for (const num of allVotes) {
-      counts[num] = counts[num] ? counts[num] + 1 : 1
+      contando[num] = contando[num] ? contando[num] + 1 : 1
+      if (contando[num] > maior) {
+        maior = contando[num]
+        setNumeroV(contando[num])
+        setMaisVotada(num)
+      }
     }
+  }
+console.log(contando)
+console.log(maisVotada)
+console.log(numeroVotos)
 
   return (
     <>
+    <div><h2>Anecdote of the day</h2></div>
+
     <div>
       {anecdotes[selected]}<br></br>
-      Has {counts[selected]}  votes
+      Has {contando[selected]}  votes
     </div>
     <div>
     <button onClick={handleVotes}>vote</button>
     <button onClick={handleRandomAnecdote}>next anecdote</button>
     </div>
+
+    <div><h2>Anecdote with most votes</h2></div>
+
+    <div>
+      {anecdotes[maisVotada]}<br></br>
+      Has {numeroVotos}  votes
+    </div>
+
+
+
     </>
   )
 }
