21e968e9739aa4cb0f1a17e7b8850b3de9ecbd25
Carlos Fabbri Jr <cfabbrijr@gmail.com>
2022-01-01 16:40:32 -0300

    Added Phonebook folder and it´s file structure and code to solve exercise_2.6 step1

---
+++ Part2/part2/phonebook/src/App.js
@@ -0,0 +1,56 @@
+// Code to exercise 2.6 Phonebook Step1
+// Developed by Carlos Fabbri Jr for FSopen2021 
+// on January 1, 2022
+
+
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  // Method to add the name inputed by user on page
+  const addName = (n) => { 
+    n.preventDefault()
+
+    // variable created to receive the name inputed by user
+    const addedName = {
+      name: newName,
+    }
+  
+    setPersons(persons.concat(addedName)) // correct way to add the name inputed on the persons array
+    setNewName('') // reset the input value field
+
+  }
+
+  // Method to handle with the change on input value fied
+  const handleNameInput = (h) => {
+    setNewName(h.target.value) 
+  }
+
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+                        value={newName} 
+                        onChange={handleNameInput}
+                />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      </div>
+    </div>
+  )
+}
+
+export default App;
