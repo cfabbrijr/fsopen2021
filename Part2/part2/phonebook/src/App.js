76056ae25f6259b0be90378546f3da8dd76f39ea
Carlos Fabbri Jr <cfabbrijr@gmail.com>
2022-01-02 01:07:55 -0300

    Added exercise_2.8 phonebook step3 code solution

--- Part2/part2/phonebook/src/App.js
+++ Part2/part2/phonebook/src/App.js
@@ -1,30 +1,34 @@
-// Code to exercise 2.6 Phonebook Step1
+// Code to exercise 2.8 Phonebook Step3
 // Developed by Carlos Fabbri Jr for FSopen2021 
-// on January 1, 2022
+// on January 2, 2022
 
 
 import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      id: 1,
+      phone: '040-1234567'}
   ]) 
   const [newName, setNewName] = useState('')
-
+  const [newPhone, setNewPhone] = useState('')
 
   // Method to add the name inputed by user on page
 
-  const addName = n => {
+  const addRecord = n => {
     n.preventDefault()
 
-  // variable created to receive the name inputed by user
-    const addedName = persons.map(person => person.name)
-    if (addedName.includes(newName.trim())) {
+  // variable created to receive the record ( name, id, phone) entered by user
+    const addedRecord = persons.map(person => person.name)
+    if (addedRecord.includes(newName.trim())) {
       alert(`${newName} is already added to phonebook`)
       setNewName('') // reset the input value field
+      setNewPhone('') // reset the phone input value field
     } else {
-      setPersons(persons.concat({ name: newName }))  // correct way to add the name inputed on the persons array
+      setPersons(persons.concat({ name: newName , id: persons.length + 1 , phone: newPhone}))  // correct way to add the record entered on the persons array
       setNewName('') // reset the input value field
+      setNewPhone('') 
     }
   }
 
@@ -32,15 +36,25 @@ const App = () => {
   const handleNameInput = (h) => {
       setNewName(h.target.value)
       }
+
+  const handlePhoneInput = (p) => {
+    setNewPhone(p.target.value)
+  }
   
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addRecord}>
         <div>
           name: <input 
                   value={newName} 
                   onChange={handleNameInput}
+                />
+        </div>
+        <div>
+          phone: <input 
+                  value={newPhone} 
+                  onChange={handlePhoneInput}
                 />
         </div>
         <div>
@@ -49,7 +63,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name} {person.phone}</p>)}
       </div>
     </div>
   )
